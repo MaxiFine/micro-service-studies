c3c232ef02e0034a162386d6eb004bddb9fd37b1
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2024-08-26 11:39:06 +0000

    trying to fix service connections:

--- department-service/src/main/java/microservices/department_service/config/WebClientConfig.java
+++ department-service/src/main/java/microservices/department_service/config/WebClientConfig.java
@@ -1,46 +1,48 @@
-//package microservices.department_service.config;
-//
-//
-//import microservices.department_service.client.EmployeeClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.reactive.function.client.WebClient;
-//import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-//import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-//
-//@Configuration
-//public class WebClientConfig {
-//
-////    @Autowired
-////    private LoadBalancedExchangeFilterFunction filterFunction;
-//
-//
-//    @Bean
-//    public WebClient employeeWebClient() {
-//        return WebClient.builder()
-//                .baseUrl("http://localhost:8082")
-//                .filter(filterFunction)
-//                .build();
-//    }
-//
+package microservices.department_service.config;
+
+
+import microservices.department_service.client.EmployeeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientConfig {
+
+    @Autowired
+    private LoadBalancedExchangeFilterFunction filterFunction;
+
+
+    @Bean
+    public WebClient employeeWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8082")
+                .filter(filterFunction)
+                .build();
+    }
+
+    @Bean
+    public EmployeeClient employeeClient() {
+        HttpServiceProxyFactory proxyFactory
+                = HttpServiceProxyFactory
+                .builder(WebClientAdapter.forClient(employeeWebClient()))
+                .build();
+        return proxyFactory.createClient(EmployeeClient.class);
+    }
+
 //    @Bean
 //    public EmployeeClient employeeClient() {
-//        HttpServiceProxyFactory proxyFactory
-//                = HttpServiceProxyFactory
-//                .builder(WebClientAdapter.forClient(employeeWebClient()))
+//        // Use the static factory method 'HttpServiceProxyFactory.create()'
+//        HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builder(WebClient.builder().baseUrl("http://localhost:8080").build())
 //                .build();
 //        return proxyFactory.createClient(EmployeeClient.class);
 //    }
-//
-////    @Bean
-////    public EmployeeClient employeeClient() {
-////        // Use the static factory method 'HttpServiceProxyFactory.create()'
-////        HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builder(WebClient.builder().baseUrl("http://localhost:8080").build())
-////                .build();
-////        return proxyFactory.createClient(EmployeeClient.class);
-////    }
-//
-//
-//}
-//
-//
+
+
+}
+
+
